9756aadf368caa54a52c2312cabc4dcb15753904
naveen joseph delphin <naveenjosephdelphin@gmail.com>
2021-10-12 21:27:51 +0530

    initial commit

---
+++ src/main/java/com/example/demo/NaveenDelp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaveenDelp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaveenDelp1Application.class, args);
+	}
+
+}
